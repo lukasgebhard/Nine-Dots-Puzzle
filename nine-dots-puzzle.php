d763bd779506e63fc22798787f19a59cee281621
Lukas Gebhard <lukas.gebhard@tf.uni-freiburg.de>
2017-10-12 09:48:28 +0200

    Wordpress shortcode; Script only loaded when needed

--- nine-dots-puzzle.php
+++ nine-dots-puzzle.php
@@ -14,6 +14,19 @@ class MrKojoNineDotsPuzzle
 {
 	static $pluginVersion = '1.0';
 	static $pluginName = 'mrkojo-nine-dots-puzzle';
+	static $enqueueScript = false;
+
+	static function handle_shortcode()
+	{
+		// Shortcode not allowed in feeds
+		if ( is_feed() ) return '';
+
+		$html = '<canvas id="canvas-nine-dots-puzzle" width="150px" height="150px"></canvas>';
+
+		self::$enqueueScript = true;
+
+		return $html; 
+	}
 
 	static function register_script()
 	{
@@ -23,18 +36,17 @@ class MrKojoNineDotsPuzzle
 
 	static function enqueue_script()
 	{
-		wp_enqueue_script( self::$pluginName, plugins_url( 'nine-dots-puzzle.js', __FILE__ ), array(), self::$pluginVersion, true );
-		//error_log('     enqueue     ', 3, '/tmp/nine-dots-puzzle-errors.log');
+		if (self::$enqueueScript)
+		{
+			wp_enqueue_script( self::$pluginName, plugins_url( 'nine-dots-puzzle.js', __FILE__ ), array(), self::$pluginVersion, true );
+		}
 	}
 
 	static function init()
 	{
-		
-		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_script' ) );
-	
-			
-
-		
+		add_shortcode( 'nine-dots-puzzle', array( __CLASS__, 'handle_shortcode' ) );
+		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'register_script' ) );
+		add_action( 'wp_footer', array( __CLASS__, 'enqueue_script' ) );
 	}
 }
 
